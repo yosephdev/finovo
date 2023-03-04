e72bc1948f6be61e4b6ff39ddea85c2430a97401
Yoseph Berhane <yosephbet@gmail.com>
2023-03-04 15:00:36 +0100

    add the necessary components to the page

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+// Footer.js
+
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <h2>Finovo</h2>
+        <p>
+          A fintech startup that offers innovative financial solutions to
+          individuals and businesses.
+        </p>
+      </div>
+      <div className="footer-links">
+        <ul>
+          <li>
+            <a href="#">Terms & Conditions</a>
+          </li>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
